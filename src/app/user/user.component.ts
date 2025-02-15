9d0dd9c88d59a3f85dc824bb0adc61fe29579040
Aletheios42 <alejandropintosdev@gmail.com>
2025-02-15 16:31:22 +0100

    Interfaz aplicada con exito conflictos entre las antes propiedades y ahora miembros resueltos

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,26 +1,31 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-type User = {
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
   id: string;
-  name: string;
   avatar: string;
-};
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // CON DECORADORES, sin señales
   @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
+
   onSelectUser() {
     this.select.emit(this.user.id);
   }
