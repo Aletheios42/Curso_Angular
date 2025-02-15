fdea14382d4d8900cf3b8a13d398c2e098b6e08d
Aletheios42 <alejandropintosdev@gmail.com>
2025-02-15 11:16:41 +0100

    Señales fuera todo por estados

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  // CON DECORADORES, sin señales
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
+  // CON SEÑALES
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  //
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+  onSelectUser() {}
 }
